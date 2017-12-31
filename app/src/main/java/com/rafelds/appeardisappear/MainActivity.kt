937816d1d5104f5edcb5dc7656177eb0e8366b47
Rafael Delos Santos <rkdelossantos@gmail.com>
2017-12-31 18:07:54 +0800

    Change long click sensor to wave sensor

--- app/src/main/java/com/rafelds/appeardisappear/MainActivity.kt
+++ app/src/main/java/com/rafelds/appeardisappear/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.rafelds.appeardisappear
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.Handler
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.view.View.GONE
+import android.view.View.VISIBLE
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_first.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     companion object {
-        private val DELAY_TO_RUN_HIDE_UI_MS = 3000L
+        private const val DELAY_TO_RUN_HIDE_UI_MS = 3000L
+        private const val SENSITIVITY = 3
     }
+
 
     private val handler = Handler()
     private val runnable = object : Runnable {
@@ -23,13 +30,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private lateinit var sensorManager: SensorManager
+    private lateinit var proximitySensor: Sensor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         UiUtils.hideSystemUI(window)
 
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
         view_pager.adapter = ImageViewPagerAdapter(supportFragmentManager)
-        view_pager.addOnPageChangeListener( object : ViewPager.OnPageChangeListener {
+        view_pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(x: Int) = Unit
             override fun onPageScrolled(x: Int, y: Float, z: Int) = Unit
             override fun onPageSelected(position: Int) {
@@ -39,11 +52,30 @@ class MainActivity : AppCompatActivity() {
             }
         })
         handler.postDelayed(runnable, DELAY_TO_RUN_HIDE_UI_MS)
+        view_pager.visibility = GONE
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
         super.onPause()
+        sensorManager.unregisterListener(this)
         finish()
     }
 
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) = Unit
+
+    override fun onSensorChanged(event: SensorEvent) {
+        val sensor = event.sensor
+        if (sensor.type == Sensor.TYPE_PROXIMITY) {
+            val sensorValue = event.values[0]
+            if (sensorValue >= -SENSITIVITY && sensorValue <= SENSITIVITY) {
+                view_pager.visibility = VISIBLE
+                sensorManager.unregisterListener(this)
+            }
+        }
+    }
 }
--- app/src/main/java/com/rafelds/appeardisappear/fragments/FirstFragment.kt
+++ app/src/main/java/com/rafelds/appeardisappear/fragments/FirstFragment.kt
@@ -1,55 +1,17 @@
 package com.rafelds.appeardisappear.fragments
 
-import android.content.Context.SENSOR_SERVICE
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.VISIBLE
 import android.view.ViewGroup
-import android.widget.ImageView
 import com.rafelds.appeardisappear.R
 
-class FirstFragment : Fragment(), SensorEventListener {
-
-    companion object {
-        const val SENSITIVITY = 4
-    }
-
-    private lateinit var sensorManager: SensorManager
-    private lateinit var proximitySensor: Sensor
-    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) = Unit
-
-    override fun onSensorChanged(event: SensorEvent) {
-        val sensor = event.sensor
-        if (sensor.type == Sensor.TYPE_PROXIMITY) {
-            val sensorValue = event.values[0]
-            if (sensorValue >= -SENSITIVITY && sensorValue <= SENSITIVITY) {
-                view?.findViewById<ImageView>(R.id.first_fragment_image)?.visibility = VISIBLE
-                sensorManager.unregisterListener(this)
-            }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        sensorManager.unregisterListener(this)
-    }
+class FirstFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        sensorManager = activity.getSystemService(SENSOR_SERVICE) as SensorManager
-        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
         return inflater?.inflate(R.layout.fragment_first, container, false)
     }
 }
